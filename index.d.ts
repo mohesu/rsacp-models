ccccf3f5341abed63b7693bb9cffbf45012387e0
Navpreet Singh <navpreetsingh951818@gmail.com>
2023-11-02 10:34:54 +0530

    some changes

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+
+import { BannerModel } from "./banner/model"; 
+import { AllPSCModel } from "./allPSC/model";
+import { DownloadModel } from "./download/model";
+import { GoverningCouncilModel } from "./governingCouncil/model";
+import { NoticeModel } from "./notice/model";
+import { MemberModel } from "./member/model";
+import { PhotoModel } from "./photo/model";
+import { RsacpAwardModel } from "./rsacpAwards/model";
+
+
+module.exports = {
+    BannerModel: BannerModel,
+    AllPSC: AllPSCModel,
+    Download: DownloadModel,
+    GoverningCouncil: GoverningCouncilModel,
+    Notice: NoticeModel,
+    Member: MemberModel,
+    Photo: PhotoModel,
+    RsacpAward: RsacpAwardModel
+}
