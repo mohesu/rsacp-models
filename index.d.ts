8a3cb6dd41dab5bda0f2c83dcccc191fee82711b
Navpreet Singh <navpreetsingh951818@gmail.com>
2023-11-03 14:26:19 +0530

    some changes

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 
-import { BannerModel } from "./banner/model"; 
-import { AllPSCModel } from "./allPSC/model";
-import { DownloadModel } from "./download/model";
-import { GoverningCouncilModel } from "./governingCouncil/model";
-import { NoticeModel } from "./notice/model";
-import { MemberModel } from "./member/model";
-import { PhotoModel } from "./photo/model";
-import { RsacpAwardModel } from "./rsacpAwards/model";
+import { BannerModel,BannerModelRequest } from "./banner/model"; 
+import { AllPSCModel,AllPSCModelRequest } from "./allPSC/model";
+import { DownloadModel,DownloadModelRequest } from "./download/model";
+import { GoverningCouncilModel,GoverningCouncilModelRequest } from "./governingCouncil/model";
+import { NoticeModel,NoticeModelRequest } from "./notice/model";
+import { MemberModel,MemberModelRequest } from "./member/model";
+import { PhotoModel,PhotoModelRequest } from "./photo/model";
+import { RsacpAwardModel,RsacpAwardModelRequest } from "./rsacpAwards/model";
